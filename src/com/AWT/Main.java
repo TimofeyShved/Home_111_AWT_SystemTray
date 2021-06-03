d8244f2db1efad8b77bfab4a1e5770fc6bb22a7c
timofeya123 <timofeya123@inbox.ru>
2021-06-03 20:28:09 +0600

    SystemTray_comment

--- src/com/AWT/Main.java
+++ src/com/AWT/Main.java
@@ -8,32 +8,34 @@ import java.awt.event.ActionListener;
 public class Main {
 
     public static void main(String[] args) throws AWTException {
-        if (SystemTray.isSupported()){
-            SystemTray tray = SystemTray.getSystemTray();
-            Image image = new ImageIcon("enot.jpeg", "desc").getImage();
-            PopupMenu popupMenu = new PopupMenu();
-            MenuItem menuItem = new MenuItem("One");
-            menuItem.addActionListener(new ActionListener() {
+        if (SystemTray.isSupported()){ // есть ли доступ к системному трею (иконки внизу)?
+
+            SystemTray tray = SystemTray.getSystemTray(); // даём ссылку на него, в переменную
+            Image image = new ImageIcon("enot.jpeg", "desc").getImage();// берем картинку для иконки
+
+            PopupMenu popupMenu = new PopupMenu(); // создаём меню
+            MenuItem menuItem = new MenuItem("One"); // поле меню
+            menuItem.addActionListener(new ActionListener() { // действие
                 @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println("one");
+                public void actionPerformed(ActionEvent e) { // активация
+                    System.out.println("one"); // вывод на экран
                 }
             });
-            popupMenu.add(menuItem);
+            popupMenu.add(menuItem); // добавляем на меню, поле
 
-            popupMenu.add(new MenuItem("two"));
-            popupMenu.add(new MenuItem("three"));
+            popupMenu.add(new MenuItem("two")); // добавляем на меню, поле
+            popupMenu.add(new MenuItem("three")); // добавляем на меню, поле
 
-            MenuItem menuItem2 = new MenuItem("exit");
-            menuItem2.addActionListener(new ActionListener() {
+            MenuItem menuItem2 = new MenuItem("exit");// поле меню
+            menuItem2.addActionListener(new ActionListener() { // действие
                 @Override
-                public void actionPerformed(ActionEvent e) {
-                   System.exit(0);
+                public void actionPerformed(ActionEvent e) {// активация
+                   System.exit(0); // выход
                 }
             });
-            popupMenu.add(menuItem2);
+            popupMenu.add(menuItem2); // добавляем на меню, поле
 
-            tray.add(new TrayIcon(image, "Tray text", popupMenu));
+            tray.add(new TrayIcon(image, "Tray text", popupMenu)); // добавляем наш трей (иконка-меню)
         }
     }
 }
